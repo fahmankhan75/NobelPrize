a8f885f529660805291966182930f2cc45710da3
Fahman Khan <f-khan@ti.com>
2020-04-06 18:45:20 -0500

    About WebPage Update

--- src/WebPages/AboutPage.js
+++ src/WebPages/AboutPage.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Navbar from '../components/Navbar/Navbar.js';
 import Footer from '../components/Footer/FooterPage.js';
+import './AboutPage.css';
 
 function AboutPage() {
     return (
     <div>
         <Navbar></Navbar>
-        <h4> Nobel Prize Facts </h4>
-        <p>The Nobel Prize is considered the most prestigious prize in the world. This prize is given in the 6 fields’ i.e, Physics, Chemistry, Medicine, Literature, Economics, and Peace Prize. These prizes were commenced in 1901. The recipients of the first Nobel Prizes were given 150,782 SEK. In 2017, the Nobel laureates were awarded a prize amount of 9,000,000 SEK.</p>
+        <img className="nobelPrizeImg" alt="Nobel Prize" src="/images/NobelPrize.jpg"/>
+        <div className="nobelInformation">
+            <h2> Nobel Prize </h2>
+            <p>The Nobel Prize is considered the most prestigious prize in the world. This prize is given in the 6 fields’ i.e, Physics, Chemistry, Medicine, Literature, Economics, and Peace Prize. These prizes were commenced in 1901. The recipients of the first Nobel Prizes were given 150,782 SEK. In 2017, the Nobel laureates were awarded a prize amount of 9,000,000 SEK.</p>
         <h4>The Prize</h4>
         <h4>The Party</h4>
         <h4>The Man</h4>
@@ -23,6 +26,7 @@ The man
 The Nobel Prize is the legacy of Sweden’s Alfred Nobel (1833–1896). A chemist, engineer, inventor and entrepreneur, Nobel was devoted to the study of explosives, and his inventions include a blasting cap, dynamite and smokeless gunpowder.
 
 When Nobel died, he held 355 patents in different countries and had made a fortune. His last will, signed in 1895, is the reason that the Nobel Prize is awarded every year. He had an idea about an annual prize that would go to brilliant minds around the world.
+        </div>
         <Footer></Footer>
     </div>
     );
